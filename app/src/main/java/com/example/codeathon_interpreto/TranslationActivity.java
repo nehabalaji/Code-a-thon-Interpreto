46ce6bdaf44ad21901c2b5f75c4194e8d7723023
nehabalaji <nehabalaji10@gmail.com>
2020-11-05 12:45:21 +0530

    add translation feature

--- app/src/main/java/com/example/codeathon_interpreto/TranslationActivity.java
+++ app/src/main/java/com/example/codeathon_interpreto/TranslationActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -19,11 +20,17 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class TranslationActivity extends AppCompatActivity {
@@ -35,6 +42,7 @@ public class TranslationActivity extends AppCompatActivity {
     private String originalText, translatedText, a;
     Bitmap imageBitmap;
     private boolean connected;
+    Translate translate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +67,8 @@ public class TranslationActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(checkInternetConnection()){
                     detectTextFromImage();
+                    getTranslateService();
+                    translate();
                 }
                 else
                 {
@@ -124,6 +134,38 @@ public class TranslationActivity extends AppCompatActivity {
         connected = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
                 connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED;
         return connected;
+    }
+
+    public void getTranslateService() {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try (InputStream is = getResources().openRawResource(R.raw.interpreto_eb585cb7eb97)) {
+
+            //Get credentials:
+            final GoogleCredentials myCredentials = GoogleCredentials.fromStream(is);
+
+            //Set credentials and get translate service:
+            TranslateOptions translateOptions = TranslateOptions.newBuilder().setCredentials(myCredentials).build();
+            translate = translateOptions.getService();
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+
+        }
+    }
+
+    public void translate() {
+
+        //Get input text to be translated:
+        originalText = textView.getText().toString();
+        Translation translation = translate.translate(originalText, Translate.TranslateOption.targetLanguage("tr"), Translate.TranslateOption.model("base"));
+        translatedText = translation.getTranslatedText();
+
+        //Translated text and original text are set to TextViews:
+        textView1.setText(translatedText);
+
     }
 
 }
